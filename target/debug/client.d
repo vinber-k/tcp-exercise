ec2c756637b4ba1fff7388ba1de35ab7eeaa789a
vinber-k <wenborao@126.com>
2021-07-25 13:45:55 +0800

    Add readme

---
+++ target/debug/client.d
@@ -0,0 +1,1 @@
+/Users/Nov/workspace/github/substrate/tcp-exercise/client/target/debug/client: /Users/Nov/workspace/github/substrate/tcp-exercise/client/src/main.rs
---
+++ target/debug/deps/client.d
@@ -0,0 +1,5 @@
+/Users/Nov/workspace/github/substrate/tcp-exercise/client/target/debug/deps/client: src/main.rs
+
+/Users/Nov/workspace/github/substrate/tcp-exercise/client/target/debug/deps/client.d: src/main.rs
+
+src/main.rs:
